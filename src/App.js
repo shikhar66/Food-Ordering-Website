f9667abd34cc11eec80cd30f80a1c28b735a7c86
Shikhar Dixit <shikhardixit20@gmail.com>
2024-07-24 00:39:31 +0530

    path correction in app.js file

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Body from "./components/Body";
 import Footer from "./components/Footer";
 
 import Error from "./components/Error";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 import RestaurantMenu from "./components/RestaurantMenu";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Profile from "./components/Profile";
